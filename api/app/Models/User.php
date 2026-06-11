eef8fc18baae1bc5fff50738925a28f7c99173d0
Luthiano Miguel <luthiano.penha@aluno.ce.gov.br>
2026-06-11 20:48:35 -0300

    feat: adicionado relação de users com posts

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Post;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
 
 #[Fillable(['name', 'email', 'password', 'telefone', 'crmv'])]
 #[Hidden(['password', 'remember_token'])]
@@ -23,5 +26,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
     }
 }
