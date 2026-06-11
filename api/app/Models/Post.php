e17f71a874d16911a10206d33ece3110d0f5a0e3
Luthiano Miguel <luthiano.penha@aluno.ce.gov.br>
2026-06-11 20:48:51 -0300

    feat: criado model de posts

--- api/app/Models/Post.php
+++ api/app/Models/Post.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    protected $fillable = ["text","publish_date","user_id"];
+    protected $fillable = [
+        'text',
+        'image_path',
+        'publish_date',
+        'user_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'publish_date' => 'date',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
